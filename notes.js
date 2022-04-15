1c57e42831b4dd58df10610f1823402475a94390
Subodh Patil <subodh@assertion.cloud>
2022-04-16 00:24:19 +0530

    added note and and remove feature

--- notes.js
+++ notes.js
@@ -1,9 +1,63 @@
 
-
-const getNotes = function(){
-    return "Notes in a string";
+const fs = require("fs");
 
 
-}
+const addNotes = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
 
-module.exports= getNotes;
+  if (duplicateNotes.length == 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log("New note added");
+  } else {
+    console.log("Note already exist");
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const getNotes = function () {
+  return "Notes in a string";
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+const removeNotes = function (title, body) {
+  const notes = loadNotes();
+
+  const removedduplicate = notes.filter(function (note) {
+    return note.title != title;
+  });
+
+  if (notes.length > removedduplicate.length) {
+    console.log("Note removed !");
+    saveNotes(removedduplicate);
+  } else {
+    console.log("Note not removed !");
+    
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNotes: addNotes,
+  removeNotes: removeNotes,
+};
